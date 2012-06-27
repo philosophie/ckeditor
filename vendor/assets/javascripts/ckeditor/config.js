046fc6be482cc351f58fe03e4a435b576774168f
patbenatar <nick@gophilosophie.com>
2012-06-27 15:41:50 -0700

    Adds Table and HR to toolbar

--- vendor/assets/javascripts/ckeditor/config.js
+++ vendor/assets/javascripts/ckeditor/config.js
@@ -8,7 +8,7 @@ CKEDITOR.editorConfig = function( config )
   // Define changes to default configuration here. For example:
   config.language = 'en';
   // config.uiColor = '#AADC6E';
-  
+
   /* Filebrowser routes */
   // The location of an external file browser, that should be launched when "Browse Server" button is pressed.
   config.filebrowserBrowseUrl = "/ckeditor/attachment_files";
@@ -18,7 +18,7 @@ CKEDITOR.editorConfig = function( config )
 
   // The location of a script that handles file uploads in the Flash dialog.
   config.filebrowserFlashUploadUrl = "/ckeditor/attachment_files";
-  
+
   // The location of an external file browser, that should be launched when "Browse Server" button is pressed in the Link tab of Image dialog.
   config.filebrowserImageBrowseLinkUrl = "/ckeditor/pictures";
 
@@ -27,23 +27,23 @@ CKEDITOR.editorConfig = function( config )
 
   // The location of a script that handles file uploads in the Image dialog.
   config.filebrowserImageUploadUrl = "/ckeditor/pictures";
-  
+
   // The location of a script that handles file uploads.
   config.filebrowserUploadUrl = "/ckeditor/attachment_files";
-  
+
   // Rails CSRF token
   config.filebrowserParams = function(){
     var csrf_token = jQuery('meta[name=csrf-token]').attr('content'),
         csrf_param = jQuery('meta[name=csrf-param]').attr('content'),
         params = new Object();
-    
+
     if (csrf_param !== undefined && csrf_token !== undefined) {
       params[csrf_param] = csrf_token;
     }
-    
+
     return params;
   };
-  
+
   config.addQueryString = function( url, params ){
     var queryString = [];
 
@@ -57,41 +57,42 @@ CKEDITOR.editorConfig = function( config )
 
     return url + ( ( url.indexOf( "?" ) != -1 ) ? "&" : "?" ) + queryString.join( "&" );
   };
-  
+
   // Integrate Rails CSRF token into file upload dialogs (link, image, attachment and flash)
   CKEDITOR.on( 'dialogDefinition', function( ev ){
     // Take the dialog name and its definition from the event data.
     var dialogName = ev.data.name;
     var dialogDefinition = ev.data.definition;
     var content, upload;
-    
+
     if ($.inArray(dialogName, ['link', 'image', 'attachment', 'flash']) > -1) {
       content = (dialogDefinition.getContents('Upload') || dialogDefinition.getContents('upload'));
       upload = (content == null ? null : content.get('upload'));
-      
+
       if (upload && upload.filebrowser['params'] == null) {
         upload.filebrowser['params'] = config.filebrowserParams();
         upload.action = config.addQueryString(upload.action, upload.filebrowser['params']);
       }
     }
   });
-  
+
   /* Extra plugins */
   // works only with en, ru, uk locales
   // config.extraPlugins = "embed,attachment";
-  
+
   // make enter create BR
   config.enterMode = CKEDITOR.ENTER_BR;
-  
+
   /* Toolbars */
   config.toolbar = 'Rundown';
-  
+
   config.toolbar_Rundown =
     [
         ['Maximize', '-', 'Source', 'Scayt'],
         ['Paste','PasteText','PasteFromWord',],
         ['Image', '-', 'SpecialChar'],
         ['Bold','Italic','Underline','Strike','-','FontSize','Subscript', 'Superscript', 'TextColor'],
-        ['NumberedList','BulletedList'],['Link','Unlink']
+        ['NumberedList','BulletedList'],['Link','Unlink'],
+        ['Table','HorizontalRule']
     ];
 };
